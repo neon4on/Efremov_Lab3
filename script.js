e0c0b67620c9d235e5c4dcaae0881e8680c2f1f0
neon4on <vkam37@gmail.com>
2024-11-11 15:23:10 +0400

    More information

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 document.addEventListener("DOMContentLoaded", function() {
-  console.log("Добро пожаловать на сайт о моей любимой книге/игре!");
+  console.log("Добро пожаловать на сайт о Age of Empires II!");
 });
